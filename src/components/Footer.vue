6f97b40a9df0003412056873c968d0bbfca77dde
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2022-07-27 08:56:26 -0300

    components navbar and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <hr />
+        <p>Footer</p>
+    </div>
+</template>
+
+<script>
+    export default {
+       name: "Footer"
+    }
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="nav">
+       <router-link to="/"><img src="" id="logo"></router-link>
+       <router-link class="home" to="/">Home</router-link>
+       <router-link to="/pedidos">Pedidos</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+    .home {
+        margin-right: 20px;
+    }
+</style>
