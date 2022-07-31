18d3dbd6e3d84b5da472305062b9938c7190e2d4
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2022-07-31 15:30:47 -0300

    resgatando dados db

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -12,35 +12,27 @@
                     <label for="pao">Pão:</label>
                     <select name="pao" id="pao" v-model="pao">
                         <option value="">-- Selecione --</option>
-                         <option value="Integral">Integral</option>
+                         <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+                            {{ pao.tipo }}
+                         </option>
                     </select>
                 </div>
 
                 <div class="input-container">
                     <label for="carne">Tipo de carne:</label>
                     <select name="carne" id="carne" v-model="carne">
-                        <option value="">-- Selecione --</option>
-                         <option value="Integral">Picanha</option>
+                        <option>-- Selecione --</option>
+                         <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+                            {{ carne.tipo }}
+                         </option>
                     </select>
                 </div>
 
                 <div id="opcionais-container" class="input-container">
                     <label id="opcionais-title" for="opcionais">Opcionais:</label>
-                   <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" value="bacon" v-model="opcionais">
-                        <span>Bacon</span>
-                   </div>
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" value="batata" v-model="opcionais">
-                        <span>Batata</span>
-                   </div>
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" value="ovo" v-model="opcionais">
-                        <span>Ovo</span>
-                   </div>
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" value="mussarela" v-model="opcionais">
-                        <span>Mussarela</span>
+                   <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id" >
+                        <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                        <span>{{ opcional.tipo }}</span>
                    </div>
                 </div>
 
@@ -54,7 +46,35 @@
 
 <script>
     export default {
-        name: "BurgerForm"
+        name: "BurgerForm",
+        data() {
+            return {
+                //dados que vem de servidor
+                paes: null,
+                carnes: null,
+                opcionaisdata: null,
+                //dados que vão pro servidor
+                nome: null,
+                pao: null,
+                carne: null,
+                opcionais: [],
+                status: "Solicitado",
+                msg: null
+            }
+        },
+        methods: {
+            async getIngredientes() {
+                const req = await fetch("http://localhost:3000/ingredientes");
+                const data = await req.json();
+
+                this.paes = data.paes;
+                this.carnes = data.carnes;
+                this.opcionaisdata = data.opcionais;
+            }
+        },
+        mounted() {
+            this.getIngredientes()
+        },
     }
 </script>
 
