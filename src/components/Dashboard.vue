d7e6659e35c9c8ceef8ddfa90d49b8c56ce66433
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2022-08-02 16:53:09 -0300

    Removendo pedidos do sistema

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -29,7 +29,7 @@
                             {{ s.tipo }}
                         </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
         </div>
@@ -61,8 +61,20 @@ import { throwStatement } from '@babel/types';
             },
             async getStatus() {
                 const req = await fetch('http://localhost:3000/status');
+
                 const data = await req.json();
+
                 this.status = data;
+
+            },
+            async deleteBurger(id) {
+                const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                    method: "DELETE"
+                });
+
+                const res = await req.json();
+
+                this.getPedidos();
 
             }
         },
